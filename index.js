8ca8171fa16bbfc6cbc10c3a075eb07bbe653f44
Sean Wilson <hello@imsean.me>
2016-10-21 18:17:15 -0500

    Added configuration option for language

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const core = require('hmu-core');
 const parse = require('hmu-runs-parse');
 
 function hmu(bot, config) {
+  config = config[hmu.name] || {};
+  const lang = config.language || 'css';
   return function run(message, args) {
     if (!args.length) return message.reply('Invalid arguments provided');
     parse(args.join(' ')).then(runs => {
@@ -11,13 +13,13 @@ function hmu(bot, config) {
           return `[${res[0]}] ${res[1]} ${res[2]}`
         });
 
-        message.channel.sendCode('toml', results.join('\n'));
+        message.channel.sendCode(lang, results.join('\n'));
       }).catch(e => console.log(e));
     }).catch(e => console.log(e));
   }
 }
 
 hmu.command = 'hmu';
-hmu.usage = 'hmu <name> [...args]';
+hmu.usage = 'hmu [...options]';
 
 module.exports = hmu;
